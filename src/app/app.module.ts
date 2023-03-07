e009c0187a6fed36ccd033fc1c772ca067d77258
Purushoth <purushothdhoni007@gmail.com>
2023-03-07 10:56:52 +0530

    initialize

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TodoComponent } from './component/todo/todo.component';
+
+import { environment } from 'src/environments/environment';
+import { AngularFireModule} from '@angular/fire/compat';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCbL7rTM1TbiuUgZ7noY5JyvjUcAxhkRZ0",
+    authDomain: "todo-app-700eb.firebaseapp.com",
+    projectId: "todo-app-700eb",
+    storageBucket: "todo-app-700eb.appspot.com",
+    messagingSenderId: "1086832439195",
+    appId: "1:1086832439195:web:3c08d19d35354a7bb4bb3e"
+  }
+};
